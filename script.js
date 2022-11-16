a427b4045628b39295deaeae35898dc043691d58
Seyit Enes <seyit7enes@gmail.com>
2022-11-16 23:14:08 +0100

    finish

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+const video = document.querySelector("video");
+const overlay = document.querySelector(".overlay");
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    if (video.paused) {
+      video.play();
+      overlay.classList.toggle("hide");
+    } else {
+      video.pause();
+      overlay.classList.toggle("hide");
+    }
+  }
+});
