6580c2a00a2bd8ff600482cdf4f788967798efce
Enes <seyit7enes@gmail.com>
2023-01-14 18:44:15 +0100

    remaking

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
 "use strict";
-const video = document.querySelector("video");
-const overlay = document.querySelector(".overlay");
-
-window.addEventListener("keydown", (e) => {
-  if (e.key === " ") {
-    if (video.paused) {
-      video.play();
-      overlay.classList.toggle("hide");
-    } else {
-      video.pause();
-      overlay.classList.toggle("hide");
-    }
-  }
-});
-
-window.addEventListener("click", () => {
-  if (video.paused) {
-    video.play();
-    overlay.classList.toggle("hide");
-  } else {
-    video.pause();
-    overlay.classList.toggle("hide");
-  }
+const video = document.getElementById("video");
+const playBtn = document.getElementById("play");
+playBtn.addEventListener("click", () => {
+  video.play();
 });
